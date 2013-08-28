128ef68710070dba975c4e72cd7c2b67e04c2128
wh1pch81n <wh1pch81n@gmail.com>
2013-08-28 14:42:23 -0700

    TT-9 Dialog view controller
    1) subclass a UIView
    2) left side will have uiimage. (this will be used to help ID who is talking)
    3) uiImage will have 5px insets from top left and bottom. the right inset will be shared with a uiscrollview

---
+++ Tean_Tytans/TTDialogViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTDialogViewController.h
+//  Tean_Tytans
+//
+//  Created by Derrick Ho on 8/28/13.
+//  Copyright (c) 2013 Derrick Ho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTDialogViewController : UIViewController
+
+@end
---
+++ Tean_Tytans/TTDialogViewController.m
@@ -0,0 +1,38 @@
+//
+//  TTDialogViewController.m
+//  Tean_Tytans
+//
+//  Created by Derrick Ho on 8/28/13.
+//  Copyright (c) 2013 Derrick Ho. All rights reserved.
+//
+
+#import "TTDialogViewController.h"
+
+@interface TTDialogViewController ()
+
+@end
+
+@implementation TTDialogViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
